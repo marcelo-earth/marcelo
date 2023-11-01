1f3d5029723f5ebebbaef8abd36c1aff799b6977
Marcelo Arias <hello@marceloarias.com>
2023-11-01 10:07:25 -0500

    Install Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://marceloarias.com',
+  integrations: [react(), tailwind(), sitemap()]
 });
